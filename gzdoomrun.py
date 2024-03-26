4db4b40a02234fdb21d4b4a92beeeefa0d3d7886
Christopher Stephen Rafuse <ImpishDeathTech@protonmail.ch>
2024-03-26 17:22:00 -0400

    Updated

--- gzdoomrun.py
+++ gzdoomrun.py
@@ -1,161 +1,175 @@
 #! /usr/bin/python3
 
-import os, sys, subprocess, importlib.util
+import importlib.util, sys, os
+import PySimpleGUI as gui 
+import gzdoomrunlib as gzdr
 
 from types import ModuleType
 from importlib.machinery import ModuleSpec
-from pathlib import Path 
+from pathlib import Path
 
-WAD_DIRECTORY     : str  = f"{Path.home()}/.config/gzdoom/"
-VERSION_MAJOR     : int  = 1
-VERSION_MINOR     : int  = 3
-VERSION_PATCH     : int  = 2
 
-CUSTOM_FILE       : dict = {
-    "name": "custom",
-    "path": f"{WAD_DIRECTORY}/custom.py"
-}
+DIRECTORY   : str = "@directory_path"
+FILES       : str = "@file_list"
+RUN_ARGS    : str = "@run_arguments"
+ARTWORK     : str = "@game_artwork"
+EXECUTE     : str = "$run_gzdoom"
+CLEAR_ARGS  : str = "$clear_arguments"
+EXIT_APP    : str = "$exit_application"
+DEFAULT_PATH: str = os.path.join(Path.home(), ".config", "gzdoom") 
 
-HELP_STRING       : str = F"""
-    GZDoom Run v{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH} Help
+class Application:
+
+    def __init__(self, title: str, file_table: list, argument_input: list):
+        self.title      : str                 = title
+        self.is_running : bool                = True
+        self.options    : gzdr.CommandOptions = gzdr.CommandOptions([])
+        self.run_args   : str                 = ""
         
-        GZDoom Run is a small linux program for loading GZDoom mods
-        a little easier. This program is meant to be used with Steam 
-        but can be used in standalone, and has a few command line options:
-    
-        help             
-            prints this message
-
-        with [keywords]   
-            searches the mod directory for any WADs or PK3s resembling the
-            provided keyword. This search is case sensitive, and is the default operation
-            when no option keyword is provided.
-        
-        list
-            Lists all of the currently available keywords.
-
-        install [file names]
-            if they exist, installs the provided WAD and PK3 files to GZdoom
-
-        remove [keywords]
-            if they exist, uninstalls the related WAD and PK3 files
-"""
-
-
-def load_custom():
-    spec   : ModuleSpec = importlib.util.spec_from_file_location(CUSTOM_FILE["name"], CUSTOM_FILE["path"])
-    custom : ModuleType = importlib.util.module_from_spec(spec)
-    
-    sys.modules[CUSTOM_FILE["name"]] = custom
-    spec.loader.exec_module(custom)
-    
-    return custom
-
-
-custom : ModuleType = load_custom()
-
-
-
-class GZDoomRunError(Exception):
-
-    def __init__(self, code: int, reason: str):
-        self.__what__ : str = f"[GZDoom Run Error] ({code}): {reason}"
-        self.__code__ : int = code
-    
-    def what(self, exit_app: bool=True):
-        print(self.__what__)
-        
-        if exit_app:
-            sys.exit(self.__code__)
-
-
-
-def find_wad(wad_name: str) -> str:
-    for file_name in os.listdir(WAD_DIRECTORY):
-        if file_name.startswith(wad_name):
-            print(file_name)
-            return f"{WAD_DIRECTORY}{file_name}"
-
-    return "nil"
-
-
-def launch_gzdoom_with(wad_list: list):
-    wad_list.insert(0, "gzdoom")
-    result : CompletedProcess = subprocess.run(wad_list)
-    
-    if result.returncode == 0:
-        print(result.stdout)
-
-    else:
-        print(result.stdout)
-        raise GZDoomRunError(result.returncode, result.stderr)
-    
-    sys.exit(result.returncode)
-
-
-def launch_gzdoom():
-    launch_gzdoom_with([])
-
-
-#########################################
-#        OOOOO PPPPP TTTTTT SSSSS       #
-#        OO OO PP PP   TT   SS          #
-#        OO OO PPPPP   TT   SSSSS       #
-#        OO OO PP      TT      SS       #
-#        OOOOO PP      TT   SSSSS       #
-#########################################
-# A set of functions to be called if    #
-# their command line option is provided #
-#########################################
-
-class CommandOptions:  
-    
-    def __init__(self, custom_options: dict):
-        self.command_options : dict = {
-            "help"   : self.print_help,
-            "with"   : self.process_arguments
+        self.events : dict = {
+            DIRECTORY : self.list_directory,
+            FILES     : self.update_image_and_args,
+            RUN_ARGS  : self.update_arglist,
+            EXECUTE   : self.run_gzdoom,
+            CLEAR_ARGS: self.clear_arguments,
+            EXIT_APP  : self.exit_application
         }
 
+        self.layout : list = [
+            [gui.Column(file_table)],
+            [gui.Column(argument_input)]
+        ]
+
+        self.window       : gui.Window = gui.Window(title=self.title, layout=self.layout)
+        self.is_first_run : bool       = True
+    
+    def find(self, event: any):
+        for key in self.events.keys():
+            if key == str(event):
+                return True
+        
+        return False
+    
+    
+    def execute(self, event: any, values: any):
+        self.events[str(event)](event, values)
+
+
+    def list_directory(self, event: any, values: any):
+        folder = values[DIRECTORY]
+        file_names: list = []
         try:
-            for key in custom_options.keys():
-                self.command_options[key] = custom_options[key]
+            file_list = os.listdir(folder)
         except:
-            return
+            file_list = []
+        finally:
+            for file_name in file_list:
+                if os.path.isfile(os.path.join(folder, file_name)) and file_name.lower().endswith(gzdr.custom.WAD_SUFFIXES):
+                    file_names.append(file_name[:-4])
 
-    def print_help(self, argc: int, argv: list):
-        print(HELP_STRING)
-        sys.exit(0)
 
+    def update_image_and_args(self, event: any, values: any):
+        try:
+            file_name = os.path.join(values[DIRECTORY], values[FILES][0])
+            run_args = values[RUN_ARGS].split(" ")
+            run_args.append(values[FILES][0])
+            self.run_args = "%".join(run_args)
+            self.window[RUN_ARGS].update(" ".join(run_args))
 
-    def process_arguments(self, argc: int, argv: list):
-        if argc > 0:
-            if argc <= 2:
-                if argv[0] == "with":
-                    print(argv[0])
-                    file_names : list = argv[1].split("%")
-                    file_paths : list = []
+        except:
+            pass
 
-                    for file_name in file_names:
-                        file_path : str = find_wad(file_name)
-
-                        if file_path != "nil":
-                            file_paths.append(file_path)
-
-                        else:
-                            raise GZDoomRunError(2, f"No '{file_path}' .WAD or .PK3 found in {WAD_DIRECTORY}")
-                    
-                    launch_gzdoom_with(file_paths)
-                
-                else:
-                    for option in self.command_options.keys():
-                        if argv[0] == option:
-                            key : str = argv[0]
-                            argv.pop(0)
-                            self.command_options[key](argc - 1, argv)
-
+    
+    def update_arglist(self, event: any, values: any):
+        if self.run_args != "":
+            self.run_args += "%"
+            self.run_args += "%".join(values[RUN_ARGS].split(" "))
+        
         else:
-            launch_gzdoom()
+            self.run_args += "%".join(values[RUN_ARGS].split(" "))
 
 
+    def run_gzdoom(self, event: any, values: any):
+        try:
+            self.options.process_arguments(2, ["with", self.run_args])
+        
+        except gzdr.GZDoomRunError as e:
+            print(e.what(False))
+        
+
+    def clear_arguments(self, event: any, values: any):
+        self.window[RUN_ARGS].update("")
+        self.run_args = ""
 
 
+    def exit_application(self, event: any, values: any):
+        self.is_running = False
+
+
+def make_application(file_names: list) -> Application:
+    return Application(f"GZDoom Run v{gzdr.VERSION_MAJOR}.{gzdr.VERSION_MINOR}.{gzdr.VERSION_PATCH}",
+    [
+        [
+            gui.Text("Mod Folder"),
+            gui.In(DEFAULT_PATH, size=(25, 1), enable_events=True, key=DIRECTORY),
+            gui.FolderBrowse()
+        ],
+        [gui.Listbox(file_names, enable_events=True, size=(49, 20), key=FILES)]
+    ],
+    [
+        [
+            gui.Text("Launch With"),
+            gui.In(size=(25, 1), enable_events=True, key=RUN_ARGS)
+        ],
+        [
+            gui.Button("Run", enable_events=True, key=EXECUTE),
+            gui.Button("Clear", enable_events=True, key=CLEAR_ARGS),
+            gui.Button("Exit", enable_events=True, key=EXIT_APP)
+        ]
+    ])
+
+
+def run_gui():
+    file_names : list = []
+
+    try:
+        file_list = os.listdir(DEFAULT_PATH)
+    except:
+        file_list = []
+    finally:
+        for file_name in file_list:
+            if os.path.isfile(os.path.join(DEFAULT_PATH, file_name)) and file_name.lower().endswith(gzdr.custom.WAD_SUFFIXES):
+                file_names.sort(key=str.lower)
+                file_names.append(file_name[:-4])
+
+    app : Application = make_application(file_names)
+
+    while app.is_running:
+        e, v = app.window.read()
+
+        if e == gui.WIN_CLOSED:
+            break
+
+        elif app.find(e):
+            app.execute(e, v)
+
+    app.window.close()
+
+
+def run_cli(argc: int, argv: list):
+    options : gzdr.CommandOptions = gzdr.CommandOptions(gzdr.custom.load_directory())
+    
+    try:
+            sys.argv.pop(0)
+            options.process_arguments(argc - 1, sys.argv)
+    
+    except gzdr.GZDoomRunError as e:
+        e.what()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        run_cli(len(sys.argv), sys.argv)
+        
+    else:
+        run_gui()
