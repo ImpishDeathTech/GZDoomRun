f28876a20cc13233150db2a881bd7ddfed89b46e
Christopher Stephen Rafuse <ImpishDeathTech@protonmail.ch>
2024-04-07 13:44:38 -0400

    Tested uninstall.py with success

--- custom/uninstall.py
+++ custom/uninstall.py
@@ -11,47 +11,49 @@ import os, json, sys, subprocess, shutil
 
 from pathlib import Path
 from subprocess import CompletedProcess
-
-MODCACHE_PATH    : str   = os.path.join(Path.home(), ".config", "gzdoom", "modcache.json")
-
-def load_modcache() -> dict:
-    with open(MODCACHE_PATH, "r") as f:
-        data = json.load(f)
-        return dict(data)
-
-def save_modcache(data: dict):
-    with open(MODCACHE_PATH, "w") as f:
-        f.truncate()
-        json.dump(data, f)
-
-modcache : dict = load_modcache()
+from gzdoomrun.utils import load_modcache, save_modcache, WAD_DIRECTORY
 
 WAD_DIRECTORY    : str   = os.path.join(Path.home(), os.path.sep.join(modcache["path"]["config"]))
 
 def main(argc: int, argv: list):
+    modcache : dict = load_modcache()
+    
     for key in modcache["manifest"].keys():
         if key == argv[0]:
+            print(f"[GZDoom Run Uninstall]: Uninstalling {key} ...")
             main_directory : str = os.path.join(WAD_DIRECTORY, key)
 
             for file_path in modcache["manifest"][key]:
+                print(f"[GZDoom Run Uninstall]: removing {file_path} ...")
+
                 if file_path.endswith(".desktop"):
                     completed : CompletedProcess = subprocess.run(["sudo", "rm", file_path])
 
                     if completed.returncode != 0:
                         print(f"[GZDoom Run Error]({completed.returncode}): {completed.stderr}")
+                        sys.exit(completed.returncode)
                 
                 elif os.path.isdir(file_path):
                     if file_path != main_directory:
                         os.rmdir(file_path)
                 
                 else:
-                    os.remove(file_path)
-            
+                    try:
+                        os.remove(file_path)
+
+                    except PermissionError:
+                        completed : CompletedProcess = subprocess.run(["sudo", "rm", file_path])
+
+                        if completed.returncode != 0:
+                            print(f"[GZDoom Run Error]({completed.returncode}): {completed.stdout}")
+                            sys.exit(completed.returncode)
+
             if os.path.isdir(main_directory):
                 os.rmdir(main_directory)
             
             del modcache["manifest"][key]
             save_modcache(modcache)
+            print(f"[GZDoom Run Uninstall]: Uninstalled {key} successfully.")
             sys.exit(0)
 
     print(f"[GZDoom Run Error]: {argv[0]} not found!")
